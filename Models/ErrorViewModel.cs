b8af448adc54eaaa02275d926487f426e640e623
tejasraut-code <rauttejas772@gamil.com>
2026-07-06 16:28:37 +0530

    First Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HomeEase_2._0_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
