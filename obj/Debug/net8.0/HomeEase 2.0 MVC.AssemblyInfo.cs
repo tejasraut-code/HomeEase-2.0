32f32298993f988f477e9ddbda87aaa2e9655dd3
tejasraut-code <rauttejas772@gamil.com>
2026-07-06 19:02:16 +0530

    _Layout Navbar Added

--- obj/Debug/net8.0/HomeEase 2.0 MVC.AssemblyInfo.cs
+++ obj/Debug/net8.0/HomeEase 2.0 MVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HomeEase 2.0 MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8af448adc54eaaa02275d926487f426e640e623")]
 [assembly: System.Reflection.AssemblyProductAttribute("HomeEase 2.0 MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HomeEase 2.0 MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
