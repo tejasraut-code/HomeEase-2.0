454206e0ec074036d8e1d5a3b6e611a60d8c6e34
tejasraut-code <rauttejas772@gamil.com>
2026-08-09 18:26:24 +0530

    add Service CRUD foundation and category dropdown setup

---
+++ Controllers/ServiceController.cs
@@ -0,0 +1,36 @@
+﻿using HomeEase_2._0_MVC.Data;
+using HomeEase_2._0_MVC.Models.DomainModels;
+using HomeEase_2._0_MVC.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace HomeEase_2._0_MVC.Controllers
+{
+    public class ServiceController : Controller
+    {
+        private readonly AppDbContext _context;
+        private readonly IWebHostEnvironment _environment;
+        public ServiceController(AppDbContext context, IWebHostEnvironment environment)
+        {
+            _context = context;
+            _environment = environment;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            ServiceViewModel viewService = new ServiceViewModel();
+            viewService.Categories = _context.Category.ToList();
+
+            SelectList categoryList = new SelectList(viewService.Categories,"CategoryId","CategoryName");
+                
+            return View(viewService);
+        }
+    }
+}
---
+++ Models/ViewModels/ServiceViewModel.cs
@@ -0,0 +1,27 @@
+﻿using HomeEase_2._0_MVC.Models.DomainModels;
+using System.ComponentModel.DataAnnotations;
+
+namespace HomeEase_2._0_MVC.Models.ViewModels
+{
+    public class ServiceViewModel
+    {
+        public int ServiceId { get; set; }
+
+        [Required]
+        public int CategoryId { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string ServiceName { get; set; } = string.Empty;
+
+        [MaxLength(500)]
+        public string Description { get; set; } = string.Empty;
+
+        [Required]
+        public decimal Price { get; set; }
+        public TimeSpan ApproximateTime { get; set; }
+        public IFormFile? BrowserImage { get; set; } 
+        public string ExistingImage { get; set; } = string.Empty;
+        public List<CategoryModel> Categories { get; set; } = new List<CategoryModel>();
+    }
+}
